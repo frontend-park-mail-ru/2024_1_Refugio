06bd0da3bfae3abc0da221547fe5303a7ec93d59
Alexey Khochenkov <alekshoch@yandex.ru>
2024-02-28 00:19:13 +0300

    ajax v2

--- public/ajax.js
+++ public/ajax.js
@@ -1,18 +1,26 @@
-function ajax(method, url, body = null, callback) {
-    const request = new Request(url, {
-        method: method,
-        body: body,
-        headers: {
-            'Content-type': 'application/json; charset=utf8'
-        }
-    });
+const ajaxWithBody = (method, url, body = null, headers, callback) => {
+    if (body) {
+        const request = new Request(url, {
+            method: method,
+            body: body,
+            headers: headers
+        });
+    }
+    else {
+        const request = new Request(url, {
+            method: method,
+            headers: headers
+        });
+    }
     fetch(request)
         .then((response) => {
             if (response.ok) {
-                return response.json()
+                return response.status, response.json()
             }
         })
-        .then((data) => {
-            callback(data);
+        .then((status, data) => {
+            callback(status, data);
         })
 }
+
+
