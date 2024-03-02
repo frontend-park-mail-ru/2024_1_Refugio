409d64afbf9d5644ba0b154b723e23d8f07776e0
AlexKhochenkov <93683589+AlexKhochenkov@users.noreply.github.com>
2024-03-02 18:49:49 +0300

    Delete public/components/header/header.precompiled.js

--- public/components/header/header.precompiled.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['header.hbs'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"header__logo\">\n    Лого\n</div>\n<div class=\"header__search\">\n    Поиск\n</div>\n<div class=\"header__menu\">\n    Меню\n    <div class=\"header__dropdown\">\n        <div class=\"header__settings\">\n            Настройки\n        </div>\n        <div class=\"header__profile\">\n            Профиль\n        </div>\n    </div>\n</div>";
-},"useData":true});
-})();
