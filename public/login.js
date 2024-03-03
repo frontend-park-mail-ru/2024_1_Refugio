4caf036feafb3ecc90428f8206f3667d6bc1f7d2
frelikhmax <frelikhmax@gmail.com>
2024-03-03 22:38:58 +0300

    added views

--- public/login.js
+++ public/login.js
@@ -2,6 +2,9 @@ import Login from './pages/login/login.js';
 
 const rootElement = document.querySelector('#root');
 
+const config = {
+};
 
-const login = new Login(rootElement);
+
+const login = new Login(rootElement, config);
 login.render();
---
+++ public/views/login.js
@@ -0,0 +1,43 @@
+import Login from '../pages/login/login'
+import BaseView from './base.js'
+import {ajax} from '../modules/ajax.js'
+
+const config = {
+    // header: {
+    //     logo: 'Logo',
+    //     search: 'Поиск',
+    //     menu: {
+    //
+    //     },
+    // },
+    // menu: {},
+    // content: {
+    //     list_letters: [
+    //         {
+    //             img: 'static/img/1200px-User_icon-cp.svg.png',
+    //             title: 'Письмо',
+    //             text: 'Some text about fish',
+    //         },
+    //         {
+    //             img: 'static/img/1200px-User_icon-cp.svg.png',
+    //             title: 'Письмо',
+    //             text: 'Some text about fish',
+    //         },
+    //         {
+    //             img: 'static/img/1200px-User_icon-cp.svg.png',
+    //             title: 'Письмо',
+    //             text: 'Some text about fish',
+    //         },
+    //     ],
+    // },
+};
+
+export default class LoginView extends BaseView {
+    renderPage() {
+        const page = new Login(this.root, config);
+        this.components.push(page);
+        this.render();
+    }
+
+
+}
