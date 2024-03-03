e6460bd6407d924923096b5c1c30c8551f14f9da
Alexey Khochenkov <alekshoch@yandex.ru>
2024-03-04 00:30:23 +0300

    cors fixed

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
 import MainView from './views/main.js';
-import LoginView from './views/main.js';
+import LoginView from './views/login.js';
 import { ajax } from '../modules/ajax.js';
 
-const isAuth = ajax(
-    'GET', 'http://89.208.223.140:8080/api/v1/verify-auth', 'application/json', (status, data) => (status === 200)
+let isAuth;
+ajax(
+    'GET', 'http://89.208.223.140:8080/api/v1/verify-auth', null, 'application/json', (status, data) => (isAuth = status === 200)
 );
 if (!isAuth) {
     const login = new LoginView();
@@ -11,4 +12,4 @@ if (!isAuth) {
 } else {
     const main = new MainView();
     main.renderPage();
-}
+};
