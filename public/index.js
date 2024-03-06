d4454dc13c91e1147ee6f7d928ad5bc87499df62
frelikhmax <frelikhmax@gmail.com>
2024-03-06 10:57:17 +0300

    signup and login error handling.repaired index.js

--- public/index.js
+++ public/index.js
@@ -3,19 +3,19 @@ import LoginView from './views/login.js';
 import ajax from '../modules/ajax.js';
 
 (async () => {
-    // const response = await ajax(
-    //     'GET', 'http://89.208.223.140:8080/api/v1/verify-auth', null, 'application/json'
-    // );
-    // const status = await response.status;
-    // const data = await response.json();
-    const status=300;
+    const response = await ajax(
+        'GET', 'http://89.208.223.140:8080/api/v1/verify-auth', null, 'application/json'
+    );
+    const status = await response.status;
+    const data = await response.json();
     if (status >= 300) {
         const login = new LoginView();
         login.renderPage();
     } else {
         const main = new MainView();
         main.renderPage();
-    };
+    }
+
 })()
 
 // let isAuth;
