4160d5bfb3dbe46c4a0332ac49dc2a9c15ee37ff
Alexey Khochenkov <alekshoch@yandex.ru>
2024-04-27 12:20:24 +0300

    some logic

--- public/pages/survey/survey.js
+++ public/pages/survey/survey.js
@@ -1,4 +1,5 @@
 import template from './survey.hbs'
+import statStore from '../../stores/statStore.js';
 
 /**
  * Класс обертки страницы
@@ -22,9 +23,27 @@ export default class Survey {
     /**
      * Рендер компонента в DOM
      */
-    render() {
+    async render() {
+        this.#config.questions = await this.#getQuestionInfo();
+        const elements = {
+            question: this.#config.questions.question[0],
+            bad: this.#config.questions.bad[0],
+            good: this.#config.questions.good[0],
+        };
         this.#parent.insertAdjacentHTML('beforeend', template());
     }
 
-   
+    async #getQuestionInfo() {
+        await dispathcher.do(actionGetQuestions());
+        return statStore.questions;
+    }
+
+    addListeners() {
+        this.#parent
+            .querySelector('.header__dropdown__logout-button')
+            .addEventListener('click', this.handleExit);
+        mediator.on('sendStat', this.handleNext);
+    }
+
+
 }
