477da19adb679ccc76fd1f9ee772f0f3373012a5
Alexey Khochenkov <alekshoch@yandex.ru>
2024-05-05 18:15:09 +0300

    drafts done

--- public/pages/write-letter/write-letter.js
+++ public/pages/write-letter/write-letter.js
@@ -1,7 +1,7 @@
 import Menu from '../../components/menu/menu.js';
 import Header from '../../components/header/header.js';
 import dispathcher from '../../modules/dispathcher.js';
-import { actionLogout, actionRedirect, actionSend } from '../../actions/userActions.js';
+import { actionLogout, actionRedirect, actionSend, actionUpdateEmail } from '../../actions/userActions.js';
 import mediator from '../../modules/mediator.js';
 import template from './write-letter.hbs'
 import router from '../../modules/router.js';
@@ -44,6 +44,9 @@ export default class Write__Letter {
         const config = this.#config;
         this.#config.menu.component = new Menu(this.#parent, this.#config.menu);
         const elements = {
+            id: this.#config.values?.id,
+            resend: this.#config.values?.resend,
+            changeDraft: this.#config.values?.changeDraft,
             sender: this.#config.values?.sender,
             date: this.#config.values?.date,
             topic: this.#config.values?.topic,
@@ -95,6 +98,64 @@ export default class Write__Letter {
         let topic = topicInput.value.trim();
         let text = textInput.value.trim();
 
+        let oldError = this.#parent
+            .querySelector('.write-letter__to__error');
+        oldError.classList.remove('appear');
+        oldError = toInput;
+        oldError.classList.remove('input-background-error');
+
+        oldError = this.#parent
+            .querySelector('.write-letter__subject__error');
+        oldError.classList.remove('appear');
+        oldError = topicInput;
+        oldError.classList.remove('input-background-error');
+
+        oldError = this.#parent
+            .querySelector('.write-letter__buttons__error');
+        oldError.classList.remove('show');
+
+        let isValidForm = true;
+        const toError = this.#parent.querySelector('.write-letter__to__error');
+        if (!to) {
+            toError.textContent = "Введите получателя";
+            toError.classList.add('appear');
+            toInput.classList.add('input-background-error');
+            isValidForm = false;
+        } else {
+            if (to.length > MAX_INPUT_LENGTH) {
+                toError.textContent = "Слишком длинное имя ящика получателя";
+                toError.classList.add('appear');
+                toInput.classList.add('input-background-error');
+                isValidForm = false;
+            } else {
+                if (to.indexOf('@') === -1) {
+                    toError.textContent = "Забыли \"@\"";
+                    toError.classList.add('appear');
+                    toInput.classList.add('input-background-error');
+                    isValidForm = false;
+                } else {
+                    if (to.indexOf('.') === -1) {
+                        toError.textContent = "Забыли \".\"";
+                        toError.classList.add('appear');
+                        toInput.classList.add('input-background-error');
+                        isValidForm = false;
+                    } else {
+                        const toRegex = /^[a-zA-Z0-9._%+-]+@mailhub.su$/;
+                        if (!toRegex.test(to)) {
+                            toError.textContent = "Некорректное имя ящика получателя";
+                            toError.classList.add('appear');
+                            toInput.classList.add('input-background-error');
+                            isValidForm = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!isValidForm) {
+            return;
+        }
+
         if (!topic) {
             topic = "Без темы";
         }
@@ -109,7 +170,7 @@ export default class Write__Letter {
             text: text,
             recipientEmail: to,
             senderEmail: this.#config.user.login,
-            draftStatus: true, 
+            draftStatus: true,
         };
         if (this.#config.values?.replyId) {
             console.log(this.#config.values.replyId);
@@ -117,7 +178,7 @@ export default class Write__Letter {
         }
         dispathcher.do(actionSend(newLetter));
     };
-    
+
 
     handleSend = async (e) => {
         e.preventDefault();
@@ -222,6 +283,201 @@ export default class Write__Letter {
         dispathcher.do(actionSend(newLetter));
     };
 
+    handleDraftUpdate = async (e) => {
+        e.preventDefault();
+        const toInput = document.querySelector('.write-letter__to__input');
+        const topicInput = document.querySelector('.write-letter__subject__input');
+        const textInput = document.querySelector('.write-letter__text');
+
+        const to = toInput.value.trim();
+        let topic = topicInput.value.trim();
+        let text = textInput.value.trim();
+
+        let oldError = this.#parent
+            .querySelector('.write-letter__to__error');
+        oldError.classList.remove('appear');
+        oldError = toInput;
+        oldError.classList.remove('input-background-error');
+
+        oldError = this.#parent
+            .querySelector('.write-letter__subject__error');
+        oldError.classList.remove('appear');
+        oldError = topicInput;
+        oldError.classList.remove('input-background-error');
+
+        oldError = this.#parent
+            .querySelector('.write-letter__buttons__error');
+        oldError.classList.remove('show');
+
+        let isValidForm = true;
+        const toError = this.#parent.querySelector('.write-letter__to__error');
+        if (!to) {
+            toError.textContent = "Введите получателя";
+            toError.classList.add('appear');
+            toInput.classList.add('input-background-error');
+            isValidForm = false;
+        } else {
+            if (to.length > MAX_INPUT_LENGTH) {
+                toError.textContent = "Слишком длинное имя ящика получателя";
+                toError.classList.add('appear');
+                toInput.classList.add('input-background-error');
+                isValidForm = false;
+            } else {
+                if (to.indexOf('@') === -1) {
+                    toError.textContent = "Забыли \"@\"";
+                    toError.classList.add('appear');
+                    toInput.classList.add('input-background-error');
+                    isValidForm = false;
+                } else {
+                    if (to.indexOf('.') === -1) {
+                        toError.textContent = "Забыли \".\"";
+                        toError.classList.add('appear');
+                        toInput.classList.add('input-background-error');
+                        isValidForm = false;
+                    } else {
+                        const toRegex = /^[a-zA-Z0-9._%+-]+@mailhub.su$/;
+                        if (!toRegex.test(to)) {
+                            toError.textContent = "Некорректное имя ящика получателя";
+                            toError.classList.add('appear');
+                            toInput.classList.add('input-background-error');
+                            isValidForm = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!isValidForm) {
+            return;
+        }
+
+        if (!topic) {
+            topic = "Без темы";
+        }
+
+        if (!text) {
+            text = "Пустое письмо";
+        }
+
+        const newLetter = {
+            readStatus: false,
+            topic: topic,
+            text: text,
+            recipientEmail: to,
+            senderEmail: this.#config.user.login,
+            draftStatus: true,
+        };
+        if (this.#config.values?.replyId) {
+            console.log(this.#config.values.replyId);
+            newLetter.replyToEmailId = this.#config.values.replyId;
+        }
+        dispathcher.do(actionUpdateEmail(this.#config.values?.id, newLetter));
+    };
+
+
+    handleSendUpdate = async (e) => {
+        e.preventDefault();
+        const toInput = document.querySelector('.write-letter__to__input');
+        const topicInput = document.querySelector('.write-letter__subject__input');
+        const textInput = document.querySelector('.write-letter__text');
+
+        const to = toInput.value.trim();
+        let topic = topicInput.value.trim();
+        let text = textInput.value.trim();
+
+        let oldError = this.#parent
+            .querySelector('.write-letter__to__error');
+        oldError.classList.remove('appear');
+        oldError = toInput;
+        oldError.classList.remove('input-background-error');
+
+        oldError = this.#parent
+            .querySelector('.write-letter__subject__error');
+        oldError.classList.remove('appear');
+        oldError = topicInput;
+        oldError.classList.remove('input-background-error');
+
+        oldError = this.#parent
+            .querySelector('.write-letter__buttons__error');
+        oldError.classList.remove('show');
+
+        // oldError = this.#parent
+        //     .querySelector('.write-letter__attachments__error');
+        // oldError.classList.remove('show');
+
+        let isValidForm = true;
+        const toError = this.#parent.querySelector('.write-letter__to__error');
+        if (!to) {
+            toError.textContent = "Введите получателя";
+            toError.classList.add('appear');
+            toInput.classList.add('input-background-error');
+            isValidForm = false;
+        } else {
+            if (to.length > MAX_INPUT_LENGTH) {
+                toError.textContent = "Слишком длинное имя ящика получателя";
+                toError.classList.add('appear');
+                toInput.classList.add('input-background-error');
+                isValidForm = false;
+            } else {
+                if (to.indexOf('@') === -1) {
+                    toError.textContent = "Забыли \"@\"";
+                    toError.classList.add('appear');
+                    toInput.classList.add('input-background-error');
+                    isValidForm = false;
+                } else {
+                    if (to.indexOf('.') === -1) {
+                        toError.textContent = "Забыли \".\"";
+                        toError.classList.add('appear');
+                        toInput.classList.add('input-background-error');
+                        isValidForm = false;
+                    } else {
+                        const toRegex = /^[a-zA-Z0-9._%+-]+@mailhub.su$/;
+                        if (!toRegex.test(to)) {
+                            toError.textContent = "Некорректное имя ящика получателя";
+                            toError.classList.add('appear');
+                            toInput.classList.add('input-background-error');
+                            isValidForm = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        const topicError = this.#parent.querySelector('.write-letter__subject__error');
+        if (topic.length > MAX_INPUT_LENGTH) {
+            topicError.textContent = "Слишком длинная тема";
+            topicError.classList.add('appear');
+            topicInput.classList.add('input-background-error');
+            isValidForm = false;
+        }
+
+        if (!isValidForm) {
+            return;
+        }
+
+        if (!topic) {
+            topic = "Без темы";
+        }
+
+        if (!text) {
+            text = "Пустое письмо";
+        }
+
+
+        const newLetter = {
+            readStatus: false,
+            topic: topic,
+            text: text,
+            recipientEmail: to,
+            senderEmail: this.#config.user.login,
+        };
+        if (this.#config.values?.replyId) {
+            console.log(this.#config.values.replyId);
+            newLetter.replyToEmailId = this.#config.values.replyId;
+        }
+        dispathcher.do(actionUpdateEmail(this.#config.values?.id, newLetter));
+    };
+
     handleDropdowns(e) {
         const target = e.target;
 
@@ -282,17 +538,27 @@ export default class Write__Letter {
             .querySelector('.header__dropdown__stat-button')
             .addEventListener('click', this.handleStat);
         this.#parent
-            .querySelector('.write-letter__buttons__send-button')
-            .addEventListener('click', this.handleSend);
-        this.#parent
             .querySelector('.write-letter__buttons__cancel-button')
             .addEventListener('click', this.handleBack);
-        this.#parent
-            .querySelector('.write-letter__buttons__save-draft-button')
-            .addEventListener('click', this.handleDraft);
+        if (this.#config.values?.changeDraft) {
+            this.#parent
+                .querySelector('.write-letter__buttons__send-button')
+                .addEventListener('click', this.handleSendUpdate);
+            this.#parent
+                .querySelector('.write-letter__buttons__save-draft-button')
+                .addEventListener('click', this.handleDraftUpdate);
+        } else {
+            this.#parent
+                .querySelector('.write-letter__buttons__send-button')
+                .addEventListener('click', this.handleSend);
+            this.#parent
+                .querySelector('.write-letter__buttons__save-draft-button')
+                .addEventListener('click', this.handleDraft);
+        }
         this.#parent.addEventListener('click', this.handleDropdowns);
         mediator.on('logout', this.handleExitResponse)
         mediator.on('send', this.handleSendResponse)
+        mediator.on('updateEmail', this.handleSendResponse)
     }
 
     /**
@@ -310,17 +576,27 @@ export default class Write__Letter {
             .querySelector('.header__dropdown__stat-button')
             .removeEventListener('click', this.handleStat);
         this.#parent
-            .querySelector('.write-letter__buttons__send-button')
-            .removeEventListener('click', this.handleSend);
-        this.#parent
             .querySelector('.write-letter__buttons__cancel-button')
             .removeEventListener('click', this.handleBack);
-        this.#parent
-            .querySelector('.write-letter__buttons__save-draft-button')
-            .removeEventListener('click', this.handleDraft);
+        if (this.#config.values?.changeDraft) {
+            this.#parent
+                .querySelector('.write-letter__buttons__send-button')
+                .removeEventListener('click', this.handleSendUpdate);
+            this.#parent
+                .querySelector('.write-letter__buttons__save-draft-button')
+                .removeEventListener('click', this.handleDraftUpdate);
+        } else {
+            this.#parent
+                .querySelector('.write-letter__buttons__send-button')
+                .removeEventListener('click', this.handleSend);
+            this.#parent
+                .querySelector('.write-letter__buttons__save-draft-button')
+                .removeEventListener('click', this.handleDraft);
+        }
         this.#parent.removeEventListener('click', this.handleDropdowns);
         mediator.off('logout', this.handleExitResponse)
         mediator.off('send', this.handleSendResponse)
+        mediator.off('updateEmail', this.handleSendResponse)
     }
 
     handleExitResponse = (status) => {
