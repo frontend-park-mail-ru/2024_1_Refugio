a29642e7f549316f9df16da9c149eac8aeaf1321
Alexey Khochenkov <alekshoch@yandex.ru>
2024-06-01 00:17:38 +0300

    fixed bug

--- public/index.js
+++ public/index.js
@@ -4,14 +4,14 @@ import '../public/index.css';
 import dispathcher from './modules/dispathcher.js';
 import { actionStart } from './actions/userActions.js';
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/sw.js', {scope: '/'})
-//     .then((reg) => {
-//         console.log('SW register', reg);
-//     })
-//     .catch((error) => {
-//         console.log('SW error', error);
-//     })
-// }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', {scope: '/'})
+    .then((reg) => {
+        console.log('SW register', reg);
+    })
+    .catch((error) => {
+        console.log('SW error', error);
+    })
+}
 
 dispathcher.do(actionStart());
